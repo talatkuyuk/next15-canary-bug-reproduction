c5214cb12272397e881f5fc6b2759d8d91b4b548
talatkuyuk <talatkuyuk@gmail.com>
2025-02-28 00:34:18 +0300

    added a prove that import declaration of react and using React.createElement is not cause of the issue

--- components/random.mjs
+++ components/random.mjs
@@ -1,5 +1,15 @@
+// Importing react here is unnecessary but aims to prove something !
+import React from "react";
+
 export default function getRandomInteger(min, max) {
   min = Math.max(0, min);
+
+  // meaningless, but just to show that importing react does not cause the issue !
+  React.createElement(
+    "strong",
+    { style: { color: "red" } },
+    "I don't cause the issue :)"
+  );
 
   const randomFraction = Math.random();
 
