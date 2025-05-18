6b15e889c41db9e8bd3ce7ac60522c17c83c0e0f
talatkuyuk <talatkuyuk@gmail.com>
2025-05-18 16:04:02 +0300

    Update Bar component

--- components/Bar.mjs
+++ components/Bar.mjs
@@ -1,10 +1,26 @@
-import React from "react";
+import * as ReactModule from "react";
 
-const Bar = () => {
-  return React.createElement(
+const Bar = ({ runtimeProps }) => {
+  const { React = ReactModule } = runtimeProps;
+
+  // for escaping pre-rendering error
+  if (!React) {
+    return "<Bar /> server component doesn't work due to missing React instance";
+  }
+
+  const id = React.useId();
+  const cre = React.createElement;
+
+  return cre(
     "div",
-    { style: { color: "blue" } },
-    "Imports work, so I am able to say HELLO from imported <Bar /> component."
+    null,
+    cre("label", { htmlFor: `${id}-name` }, "Enter your id : "),
+    cre("input", {
+      id: `${id}-name`,
+      type: "text",
+      placeholder: String(id).slice(1, -1),
+      style: { paddding: "5px" },
+    })
   );
 };
 
